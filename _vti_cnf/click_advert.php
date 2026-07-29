3d55d121411072e9c67d9c467f692b8ea45be347
Gregary Boyles <gregplants@bigpond.com>
2026-07-29 15:39:14 +1000

    Add files via upload

---
+++ _vti_cnf/click_advert.php
@@ -0,0 +1,15 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|LATITUDE-E5430\\gregaryb
+vti_modifiedby:SR|LATITUDE-E5430\\gregaryb
+vti_timelastmodified:TR|23 May 2026 18:19:41 -0000
+vti_timecreated:TR|22 May 2026 09:21:41 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_nexttolasttimemodified:TW|23 May 2026 18:18:23 -0000
+vti_cacheddtm:TX|23 May 2026 18:19:41 -0000
+vti_filesize:IR|406
+vti_cachedneedsrewrite:BR|false
+vti_cachedhasbots:BR|false
+vti_cachedhastheme:BR|false
+vti_cachedhasborder:BR|false
+vti_charset:SR|utf-8
---
+++ _vti_cnf/text_restriction.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|LATITUDE-E5430\\gregaryb
+vti_modifiedby:SR|LATITUDE-E5430\\gregaryb
+vti_timelastmodified:TR|11 Apr 2026 11:38:24 -0000
+vti_timecreated:TR|18 Sep 2025 08:25:11 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_nexttolasttimemodified:TW|11 Apr 2026 11:37:55 -0000
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|11 Apr 2026 11:38:24 -0000
+vti_filesize:IR|2909
